59ff5714e1e432001bf1fb03dd90179d3100eaad
Unai <irunavarro@lhusurbil.eus>
2026-08-23 10:26:25 +0200

    feature/koloreaAldatzen commiteatzen

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+    public static final String KOLOREA= "\u001B[32m";
+    public static final String RESET="\u001B[0m";
     public static void main(String[] args) {
-        System.out.println("Kaixo, honako hau testu bat da.");
+        System.out.println(KOLOREA+"Kaixo, honako hau testu bat da."+RESET);
     }
 }
